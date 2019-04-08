261f9da5b72014f70e95a07b183a95b127d871ee
thirunavukkarasu95 <thirunavukkarasu.95@gmail.com>
2019-04-07 21:10:46 -0400

    Added Pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-import Like from "../common/like.jsx";
+import Like from "./common/like.jsx";
+import Pagination from "./common/pagination";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    pageSize: 4,
+    currentPage: 1
   };
 
   handleDelete = movie => {
@@ -20,8 +23,13 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
     if (count === 0) return "There are no movies in the database!";
     return (
       <React.Fragment>
@@ -62,6 +70,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+          currentPage={currentPage}
+        />
       </React.Fragment>
     );
   }
